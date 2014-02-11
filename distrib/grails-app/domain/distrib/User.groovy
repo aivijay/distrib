e4e3fa7d3e59328d726bff29f93af16f6645f8b2
Vijay Krishnan <aivijay@gmail.com>
2014-02-10 18:30:33 -0600

    Added User domain class

---
+++ distrib/grails-app/domain/distrib/User.groovy
@@ -0,0 +1,27 @@
+package distrib
+
+class User {
+	String firstName
+	String lastName
+	String userName
+	String password
+	String email
+	String confirm
+
+	static constraints = {
+		userName(blank:false,unique:true)
+		password(blank:false, minLength:3)
+
+		email(email:true)
+		firstName(blank:false)
+		lastName(blank:false)
+	}
+
+	String  toString () {
+		return "$lastName, $firstName"
+	}
+
+	String dump() {
+		return "FirstName: ${firstName}, LastName: ${lastName}, UserName: ${userName},Password: ${password},  Email: ${email}"
+	}
+}
